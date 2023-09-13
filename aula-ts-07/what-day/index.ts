58ddc5eaac4253d2a54c65f58cc161e78a8d08f1
thiago-arend <thiago.arend@gmail.com>
2023-09-13 19:58:33 -0300

    passo a passo para configurar o ambiente

--- aula-ts-07/what-day/index.ts
+++ aula-ts-07/what-day/index.ts
@@ -9,12 +9,39 @@ app.get("/today", (req: Request, res: Response) => {
   })
 });
 
-const port = process.env.PORT || 5000;
+const port: number = Number(process.env.PORT) || 5000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
 
-/* **************** como resolver "ReferenceError: exports is not defined in ES module scope" ******************** */
-/* **************** remoção do "type": "module" ******************** */
+/*
 
-/* **************** OU ******************** */
+instalar os pacotes necessários:
+  npm i -D typescript ts-node nodemon @types/express @types/node
+  npm i express ...... e todos os demais pacotes para api's
 
-/* **************** adição da linha "module": "es6" no tsconfig.json ******************** */
+executar
+  npx tsc --init                        --> gera o tsconfig.json
+
+e usar a configuração:
+  {
+    "compilerOptions": {
+      "outDir": "dist",
+      "esModuleInterop": true,
+      "module": "CommonJS",
+      "moduleResolution": "Node",
+      "baseUrl": "src",
+    },
+    "ts-node": {
+      "esm": true
+    },
+  }
+
+alterar os scripts do package.json:     --> para rodar no ambiente ts (as pastas dependem do jeito que está desenvolvendo)
+  "scripts": {
+      "start": "node ./dist/index.js",
+      "dev": "nodemon ./src/index.ts"
+    },
+
+remover do package.json a linha:        --> é necessário remover, pois a informação de módulo já está presente no ts config.json e evita o erro "ReferenceError: exports is not defined in ES module scope
+    “type”:”module”
+
+*/
